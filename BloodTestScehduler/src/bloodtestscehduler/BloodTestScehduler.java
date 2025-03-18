ab38479eb66379d29f373ed616725060a91a5d42
Edvinkk <70326405+Edvinkk@users.noreply.github.com>
2025-03-18 18:39:30 +0000

    initial commit

---
+++ BloodTestScehduler/src/bloodtestscehduler/BloodTestScehduler.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bloodtestscehduler;
+
+/**
+ *
+ * @author edvin
+ */
+public class BloodTestScehduler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BloodTestSchedulerGUI myGUI = new BloodTestSchedulerGUI();
+        myGUI.setVisible(true);
+    }
+    
+}
